2d6536f626f473ff9748b2eac6181a7ec245121b
Samuelodu1 <samuelodu88@gmail.com>
2024-11-01 15:16:22 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'samuelodu1/wallet-sdk',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ee4c26d95d6ea7efcc4022d8984f0f31fe26668',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'samuelodu1/wallet-sdk' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ee4c26d95d6ea7efcc4022d8984f0f31fe26668',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
